26f01bb53d9c62a73a5acac61a557b8234dc2300
Nikolay Fadin <kuvaldis1988@gmail.com>
2016-02-20 17:54:20 +0300

    some haskell play

--- src/baby.hs
+++ src/baby.hs
@@ -48,3 +48,59 @@ recursiveSum (head:tail) = head + recursiveSum tail
 capital :: String -> String
 capital "" = "Empty string"
 capital input@(head:tail) = "The first letter of " ++ input ++ " is " ++ [head]
+
+-- guards
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat = "You're fat! Lose some weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+-- where
+-- pattern matching also works
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+-- inline
+max' :: (Ord a) => a -> a -> a
+max' a b | a > b = a | otherwise = b
+-- compare
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b = GT
+    | a == b = EQ
+    | otherwise = LT
+
+-- where
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+describeListWhere :: [a] -> String
+describeListWhere xs = "This is " ++ what xs
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
+
+-- list of tuples of two real floats
+calcBims :: (RealFloat a) => [(a, a)] -> [a]
+calcBims xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+-- let binding
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+-- unsafe use let with list comprehensions
+calcBims1 :: (RealFloat a) => [(a, a)] -> [a]
+calcBims1 xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+-- case
+head'1 :: [a] -> a
+head'1 xs = case xs of [] -> error "No head for empty lists!"
+                       (x:_) -> x
+describeListCase :: [a] -> String
+describeListCase xs = "This is " ++ case xs of [] -> "empty."
+                                               [x] -> "a singleton list."
+                                               xs -> "a longer list."
