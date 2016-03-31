8658041dd00a08adffec66b2e7d1eee5f7cdfc9d
Nikolay Fadin <kuvaldis1988@gmail.com>
2016-03-31 12:59:47 +0300

    typeclasses

--- src/modules.hs
+++ src/modules.hs
@@ -36,3 +36,28 @@ search needle haystack =
     let nlen = length needle
     -- tails: "ABC" -> ["ABC", "BC", "C", ""]
     in  foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+
+data Maybe a = Nothing | Just a deriving (Show, Eq, Ord)
+-- Nothing < Just 100 true
+-- Just 100 > Just 50 true
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectMult` m = Vector (i * m) (j * m) (k * m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i * l + j * m + k * n
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     } deriving (Eq, Show, Read)
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
+-- Bounded minBound :: Day == Monday
+-- Bounded maxBound :: Day == Sunday
