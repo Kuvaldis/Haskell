55452fa06937bc3c11fe3a9318f847f3055094e7
Kuvaldis <kuvaldis1988@gmail.com>
2017-03-06 23:43:44 +0300

    instance declaration

--- src/types.hs
+++ src/types.hs
@@ -23,3 +23,15 @@ lockers = Map.fromList
         ,(109,(Taken,"893JJ"))
         ,(110,(Taken,"99292"))
         ]
+
+
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+    Red == Red = True
+    Yellow == Yellow = True
+    Green == Green = True
+    _ == _ = False
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
