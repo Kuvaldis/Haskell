3a97bcc46199d3cee539f224608ac14ab9407053
Nikolay Fadin <Nikolay_Fadin@epam.com>
2017-10-09 13:23:18 +0300

    bytestrings

---
+++ src/IO/bytestrings.hs
@@ -0,0 +1,6 @@
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString as S
+
+-- creates lazy bytestring from a list of strict ones
+testFromChunk :: B.ByteString
+testFromChunk = B.fromChunks [S.pack [40, 41, 42], S.pack [43, 44, 45], S.pack [46, 47, 48]]
